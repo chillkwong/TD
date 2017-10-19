d453f15924b605ae3f2f1f8bceaa7b8f16f75194
chill kwong <chillkwong@chills-MacBook-Pro.local>
2017-10-19 14:01:04 +0800

    index web eng

--- resources/views/layouts/frontHeader.blade.php
+++ resources/views/layouts/frontHeader.blade.php
@@ -393,8 +393,8 @@
 </section>
  </div>
 
-<script type="text/javascript" src="{{mix('js/app.js')}}"></script> 
-
+<!-- <script type="text/javascript" src="{{mix('js/app.js')}}"></script> 
+ -->
 <script type="text/javascript">
 
 
